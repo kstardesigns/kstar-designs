2aa52b2357d7e77cc64aa855393acd0d3e15d9ea
Kyle Stark <kyleastark@gmail.com>
2016-06-29 08:32:07 -0700

    Added kstar v3 folder and darwins 6-28 email updates

---
+++ v3/js/scripts.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$('.menu-button').click(function(){
+    $('nav').toggleClass('open');
+    $('nav ul').toggle();
+    $('.menu').toggleClass('close-icon');
+});
+
+//$(document).ready(function() {
+   
+
+//});
+
+
