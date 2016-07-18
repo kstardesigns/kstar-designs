5c8d0f80113950ac39a769fbf786c2008a05eb78
Kyle Stark <kyleastark@gmail.com>
2016-07-18 16:24:12 -0700

    Portraits page and tabs

---
+++ kelsie/js/scripts.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+$('.tab1').click(function(){
+    $('.tab1').addClass('activetab');
+    $('.portraits-pics').addClass('active');
+    $('.main > div:not(".portraits-pics")').removeClass('active');
+    $('.portrait-toggle span:not(.tab1)').removeClass('activetab');
+});
+
+$('.tab2').click(function(){
+    $('.tab2').addClass('activetab');
+    $('.portraits2-pics').addClass('active');
+    $('.main > div:not(".portraits2-pics")').removeClass('active');
+    $('.portrait-toggle span:not(.tab2)').removeClass('activetab');
+});
+
+$('.tab3').click(function(){
+    $('.tab3').addClass('activetab');
+    $('.portraits3-pricing').addClass('active');
+    $('.main > div:not(".portraits3-pricing")').removeClass('active');
+    $('.portrait-toggle span:not(.tab3)').removeClass('activetab');
+});
+
+
+
+//$(document).ready(function() {
+   
+
+//});
+
+
