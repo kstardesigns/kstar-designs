6b2cb464ee7a99471697d22e42acd0ac69aac4d0
Kyle Stark <kyleastark@gmail.com>
2016-07-31 22:56:58 -0700

    Last fixes for v3

--- v3/js/scripts.js
+++ v3/js/scripts.js
@@ -16,32 +16,38 @@ var portfolioItems = [
 		{
 			'proj': 'leslie',
 			'itemname': 'Leslie Stark, Realtor',
-			'itemdescription': 'A single-page realtor website developed with a tabbed design. <a href="http://lesliestarkaz.com" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A single-page realtor website developed with a tabbed design. <a href="http://lesliestarkaz.com" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://lesliestarkaz.com'
 		},
 		{
 			'proj': 'whogotlucilled',
 			'itemname': 'Who Got Lucille\'d?',
-			'itemdescription': 'A fansite for <i>The Walking Dead</i> allowing fans to vote regarding the 6th season finale. <a href="http://whogotlucilled.com" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A fansite for <i>The Walking Dead</i> where fans can vote about the cliffhanger from the 6th season finale. <a href="http://whogotlucilled.com" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://whogotlucilled.com'
 		},
 		{
 			'proj': 'cbc',
 			'itemname': 'CBC, Inc.',
-			'itemdescription': 'A custom website developed for a luxury home builder in Mesa, AZ. <a href="http://cbcarizona.com" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A custom website developed for a luxury home builder in Mesa, AZ. <a href="http://cbcarizona.com" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://cbcarizona.com'
 		},
 		{
 			'proj': 'thewikifix',
 			'itemname': 'The WikiFix',
-			'itemdescription': 'A random fact-generating website consisting of information taken from Wikipedia. <a href="http://thewikifix.com" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A random fact-generating website consisting of information taken from Wikipedia. <a href="http://thewikifix.com" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://thewikifix.com'
 		},
 		{
 			'proj': 'clayton',
 			'itemname': 'Clayton Sullivan Portfolio',
-			'itemdescription': 'A portfolio website displaying the work of a stuntman/actor. <a href="http://claytonasullivan.com" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A portfolio website displaying the work of a stuntman/actor. <a href="http://claytonasullivan.com" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://claytonasullivan.com'
 		},
 		{
 			'proj': 'sandman',
 			'itemname': 'Charity Golf Scramble',
-			'itemdescription': 'A single-page website giving information for a charity golf event. <a href="http://kstardesigns.com/sandman" target="_blank" title="">View site <span>&rarr;</span></a>'
+			'itemdescription': 'A single-page website giving information for a charity golf event. <a href="http://kstardesigns.com/sandman" target="_blank" title="">View site <span>&rarr;</span></a>',
+			'projlink': 'http://kstardesigns.com/sandman'
 		}
 	];
 
@@ -74,6 +80,7 @@ $(document).ready(function() {
    $('#project').addClass(portfolioItems[portfolioNumber].proj);
    $('.item-name').html(portfolioItems[portfolioNumber].itemname);
    $('.item-description').html(portfolioItems[portfolioNumber].itemdescription);
+   $('.screen a.projlink').attr('href', portfolioItems[portfolioNumber].projlink);
 
 });
 
@@ -81,5 +88,6 @@ function showPortfolio(){
 	$('#project').removeClass().addClass(portfolioItems[portfolioNumber].proj);
 	$('.item-name').html(portfolioItems[portfolioNumber].itemname);
 	$('.item-description').html(portfolioItems[portfolioNumber].itemdescription);
+    $('.screen a.projlink').attr('href', portfolioItems[portfolioNumber].projlink);
 }
 
