056383807657b477ddc5ddcd6dff7286a2705884
Kyle <kyleastark@gmail.com>
2017-04-11 15:46:55 -0700

    Revert "Random updates"
    This reverts commit cbc42d8c5298cb0f74d1faf929d604164e0c6319.

--- js/scripts.js
+++ js/scripts.js
@@ -16,49 +16,49 @@ var portfolioItems = [
 		{
 			'proj': 'cbc',
 			'itemname': 'CBC, Inc.',
-			'itemdescription': 'A custom website developed for a luxury home builder in Mesa, AZ. <a href="http://cbcarizona.com" target="_blank" title="CBC, Inc.">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A custom website developed for a luxury home builder in Mesa, AZ. <a href="http://cbcarizona.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://cbcarizona.com'
 		},
 		{
 			'proj': 'darwins',
 			'itemname': 'Darwin\'s Natural',
-			'itemdescription': 'A custom website built for a medicinal marijuana wholesale company. <a href="http://darwinsnatural.com" target="_blank" title="Darwin\'s Natural">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A custom website built for a medicinal marijuana wholesale company. <a href="http://darwinsnatural.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'darwins/index.html'
 		},
 		{
 			'proj': 'titledrops',
 			'itemname': 'Title Drops',
-			'itemdescription': 'A project that tells when a TV show or movie mentions its own title in its script. <a href="http://titledrops.xyz" target="_blank" title="Title Drops">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A project that tells when a TV show or movie mentions its own title in its script. <a href="http://titledrops.xyz" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://titledrops.xyz'
 		},
 		{
 			'proj': 'leslie',
 			'itemname': 'Leslie Stark, Realtor',
-			'itemdescription': 'A single-page realtor website developed with a tabbed design. <a href="http://lesliestarkaz.com" target="_blank" title="Leslie Stark, Realtor">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A single-page realtor website developed with a tabbed design. <a href="http://lesliestarkaz.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://lesliestarkaz.com'
 		},
 		{
 			'proj': 'thewikifix',
 			'itemname': 'The WikiFix',
-			'itemdescription': 'A random fact-generating website consisting of information taken from Wikipedia. <a href="http://thewikifix.com" target="_blank" title="The Wikifix">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A random fact-generating website consisting of information taken from Wikipedia. <a href="http://thewikifix.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://thewikifix.com'
 		},
 		{
 			'proj': 'whogotlucilled',
 			'itemname': 'Who Got Lucille\'d?',
-			'itemdescription': 'A fansite for <i>The Walking Dead</i> where fans can vote about the cliffhanger from the 6th season finale. <a href="http://whogotlucilled.com" target="_blank" title="Who Got Lucille\'d?">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A fansite for <i>The Walking Dead</i> where fans can vote about the cliffhanger from the 6th season finale. <a href="http://whogotlucilled.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://whogotlucilled.com'
 		},
 		{
 			'proj': 'clayton',
 			'itemname': 'Clayton Sullivan Portfolio',
-			'itemdescription': 'A portfolio website displaying the work of a stuntman/actor. <a href="http://claytonasullivan.com" target="_blank" title="Clayton Sullivan Portfolio">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A portfolio website displaying the work of a stuntman/actor. <a href="http://claytonasullivan.com" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'http://claytonasullivan.com'
 		},
 		{
 			'proj': 'sandman',
 			'itemname': 'Charity Golf Scramble',
-			'itemdescription': 'A single-page website giving information for a charity golf event. <a href="http://kstardesigns.com/sandman" target="_blank" title="Charity Golf Scramble">View site <span>&rarr;</span></a>',
+			'itemdescription': 'A single-page website giving information for a charity golf event. <a href="http://kstardesigns.com/sandman" target="_blank" title="">View site <span>&rarr;</span></a>',
 			'projlink': 'sandman/index.html'
 		}
 	];
@@ -86,26 +86,30 @@ $('.left').click(function(){
 });
 
 /* Scroll transition to anchor */
+
 $('.godown').click(function() {
-  $('*').animate({
-    scrollTop: $("#skills").offset().top },
-    '1500'
-  );
-  // Prevent default behavior of link
-  return false;
+
+$('*').animate({
+  scrollTop: $("#skills").offset().top },
+  '1500'
+);
+// Prevent default behavior of link
+return false;
 });
 
 
 $(document).ready(function() {
+
    $('#project').addClass(portfolioItems[portfolioNumber].proj);
    $('.item-name').html(portfolioItems[portfolioNumber].itemname);
    $('.item-description').html(portfolioItems[portfolioNumber].itemdescription);
    $('.screen a.projlink').attr('href', portfolioItems[portfolioNumber].projlink);
+
 });
 
 function showPortfolio(){
-  $('#project').removeClass().addClass(portfolioItems[portfolioNumber].proj);
-  $('.item-name').html(portfolioItems[portfolioNumber].itemname);
-  $('.item-description').html(portfolioItems[portfolioNumber].itemdescription);
-  $('.screen a.projlink').attr('href', portfolioItems[portfolioNumber].projlink);
+	$('#project').removeClass().addClass(portfolioItems[portfolioNumber].proj);
+	$('.item-name').html(portfolioItems[portfolioNumber].itemname);
+	$('.item-description').html(portfolioItems[portfolioNumber].itemdescription);
+    $('.screen a.projlink').attr('href', portfolioItems[portfolioNumber].projlink);
 }
