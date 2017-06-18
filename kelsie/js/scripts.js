281c5035ce4fd3ca2adb94442899f5e763defafa
Kyle Stark <kyleastark@gmail.com>
2017-06-18 11:20:24 -0700

    Updates to /kelsie to match kstarkphoto.com

--- kelsie/js/scripts.js
+++ kelsie/js/scripts.js
@@ -21,6 +21,20 @@ $('.tab3').click(function(){
     $('.portrait-toggle span:not(.tab3)').removeClass('activetab');
 });
 
+$('.tab4').click(function(){
+    $('.tab4').addClass('activetab');
+    $('.portraits4-psalms').addClass('active');
+    $('.main > div:not(".portraits4-psalms")').removeClass('active');
+    $('.portrait-toggle span:not(.tab4)').removeClass('activetab');
+});
+
+$('.tab5').click(function(){
+    $('.tab5').addClass('activetab');
+    $('.portraits5-pics').addClass('active');
+    $('.main > div:not(".portraits5-pics")').removeClass('active');
+    $('.portrait-toggle span:not(.tab5)').removeClass('activetab');
+});
+
 $('.inside nav:not(ul)').click(function(){
     $('.inside nav ul').toggle();
     $('.inside .navmenu').toggleClass('open');
@@ -28,8 +42,6 @@ $('.inside nav:not(ul)').click(function(){
 
 
 //$(document).ready(function() {
-   
+
 
 //});
-
-
