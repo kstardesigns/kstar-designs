0524ea71cbdc6a875740ed49c4b576fb15803767
Kyle Stark <kstark@prizelogic.net>
2018-04-24 13:15:05 -0700

    First graph implemented

--- the-office/scripts.js
+++ the-office/scripts.js
@@ -1,4 +1,6 @@
-var props = {
+//start of bar graph
+
+var props1 = {
   "chart":{"label":{"x":"season","y":"count"}},
   "svg":{
     "width": 800,"height":560,
@@ -14,7 +16,7 @@ var props = {
   }
 };
 
-var data = [
+var data1 = [
     {"season":"1", "count":8},
     {"season":"2", "count":27},
     {"season":"3", "count":40},
@@ -26,44 +28,43 @@ var data = [
     {"season":"9", "count":18}
 ];
 
-let labelX = props.chart.label.x,
-    labelY = props.chart.label.y;
+let labelX1 = props1.chart.label.x,
+    labelY1 = props1.chart.label.y;
 
-
-var div = d3.select("#tooltip").html(props.tooltip.content);
+var div1 = d3.select("#tooltip").html(props1.tooltip.content);
 
 //apply tooltip box styles
-for(let key in props.tooltip.styles){
-  div.style(key, props.tooltip.styles[key]);
+for(let key in props1.tooltip.styles){
+  div1.style(key, props1.tooltip.styles[key]);
 }
 
-var margin = {top: 20, right: 20, bottom: 80, left: 40},
-    width = props.svg.width - margin.left - margin.right,
-    height = props.svg.height - margin.top - margin.bottom;
+var margin1 = {top: 20, right: 20, bottom: 80, left: 40},
+    width1 = props1.svg.width - margin1.left - margin1.right,
+    height1 = props1.svg.height - margin1.top - margin1.bottom;
 
-var svg = d3.select("svg").attr("width",props.svg.width)
-                          .attr("height", props.svg.height);
+var svg1 = d3.select("svg").attr("width",props1.svg.width)
+                          .attr("height", props1.svg.height);
 
 //set svg styles
-for(let key in props.svg.styles){
-  svg.style(key, props.svg.styles[key]);
+for(let key in props1.svg.styles){
+  svg1.style(key, props1.svg.styles[key]);
 }
 
-var x = d3.scaleBand().rangeRound([0,width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height,0]);
+var x1 = d3.scaleBand().rangeRound([0,width1]).padding(0.1),
+    y1 = d3.scaleLinear().rangeRound([height1,0]);
 
-var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var g1 = svg1.append("g")
+        .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
 
-x.domain(data.map(d=>d[labelX]));
-y.domain([0, d3.max(data, d=>d[labelY])]);
+x1.domain(data1.map(d=>d[labelX1]));
+y1.domain([0, d3.max(data1, d=>d[labelY1])]);
 
-g.append("g")
- .call(d3.axisLeft(y));
+g1.append("g")
+ .call(d3.axisLeft(y1));
 
-g.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
+g1.append("g")
+  .attr("transform", "translate(0," + height1 + ")")
+  .call(d3.axisBottom(x1))
   .selectAll("text")
   .style("text-anchor", "end")
   .attr("dx", "-.8em")
@@ -71,31 +72,31 @@ g.append("g")
   .attr("transform", "rotate(-90)" );
 
 
-g.selectAll("rect")
-  .data(data)
+g1.selectAll("rect")
+  .data(data1)
   .enter().append("rect")
-          .attr("x", d=>18+x(d[labelX]))
-          .attr("y", d=>y(d[labelY]))
-          .attr("width", .5*(x.bandwidth()))
-          .attr("height", d=>height-y(d[labelY]))
-          .style("fill",props.bar.fill)
+          .attr("x", d=>18+x1(d[labelX1]))
+          .attr("y", d=>y1(d[labelY1]))
+          .attr("width", .5*(x1.bandwidth()))
+          .attr("height", d=>height1-y1(d[labelY1]))
+          .style("fill",props1.bar.fill)
           .on("mouseover",handleMouseOver)
           .on("mousemove", showTooltip)
           .on("mouseout",handleMouseOut);
 
 
-svg.append("g")
+svg1.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+    .attr("transform", "translate(0," + height1 + ")")
     //.call(xAxis)
 		.append("text")
 		.attr("class", "axis-label")
 		.attr("y", 50)
-		.attr("x", (.5*width)-30)
+		.attr("x", (.5*width1)-30)
 		.text('Season');
 
 function handleMouseOver(d,i){
-  d3.select(this).style("fill",props.bar.hover_fill);
+  d3.select(this).style("fill",props1.bar.hover_fill);
 }
 
 function showTooltip(d,i){
@@ -106,18 +107,19 @@ function showTooltip(d,i){
   // let p = {x: d3.event.pageX-svgObj.getBoundingClientRect().x,
   //          y: d3.event.pageY-svgObj.getBoundingClientRect().y};
 
-  let content = props.tooltip.content
-                  .replace("${" + labelX + "}",d[labelX])
-                  .replace("${" + labelY + "}",d[labelY]);
+  let content = props1.tooltip.content
+                  .replace("${" + labelX1 + "}",d[labelX1])
+                  .replace("${" + labelY1 + "}",d[labelY1]);
 
-  div.html(content);
-  div.style("display","block").style("position", "absolute")
+  div1.html(content);
+  div1.style("display","block").style("position", "absolute")
       .style("left", (d3.event.pageX - 40) + "px")
       .style("top", (d3.event.pageY + 20) + "px");
-
 }
 
 function handleMouseOut(d,i){
-  d3.select(this).style("fill", props.bar.fill);
+  d3.select(this).style("fill", props1.bar.fill);
   div.style("display","none");
 }
+
+//start of line graph
