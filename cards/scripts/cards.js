01877e3127eade3b7bb3e4f8d935182c1cc1c277
Kyle <kstrk@prizelogic.com>
2019-03-06 11:03:08 -0700

    Style updates for 2019 card

--- cards/scripts/cards.js
+++ cards/scripts/cards.js
@@ -14,7 +14,6 @@ new Vue({
 				recordsType: 'pitching',
 				rookie: true,
 				cardStyle: 'topps2001',
-				toppsPosition: 'top-left',
 				cardNo: 792,
 				sources: 'Pitch, episodes 1, 3-4, 6-8, 10',
 			 	statsYear1: {
@@ -34,7 +33,6 @@ new Vue({
 				position: 'DH',
 				recordsType: 'batting',
 				cardStyle: 'topps2001',
-				toppsPosition: 'top-left',
 				cardNo: 793,
 				sources: 'It\'s Always Sunny in Philadelphia, episode 10x1 "The Gang Beats Boggs"',
 				// isHorizontal: true,
@@ -55,7 +53,6 @@ new Vue({
 				position: 'LF',
 				recordsType: 'batting',
 				cardStyle: 'topps2001',
-				toppsPosition: 'top-left',
 				cardNo: 794,
 				sources: 'The Complete Calvin and Hobbes, comics dated 6/22/86, 8/1/86, 2/10/87, 6/10/87, 8/9/87, 5/22/88, 6/9/88, 6/19/88, 7/17/88, 8/6/89, 8/24/89, 5/1/90, 6/24/90, 7/22/90, 8/8/90, 5/23/93, 12/12/93, 3/15/95',
 				// isHorizontal: true,
@@ -83,6 +80,37 @@ new Vue({
 				totals: {
 						year: 'totals', g: 14, ab: 11, h: 8, '2b': 1, '3b': 1, hr: 3, rbi: 3, sb: 3, so: 2, avg: '.727'
 				}
+			},
+			{
+				name: 'Kyle Stark',
+			  string: 'kylestark',
+				media: 'MLB The Show 18',
+			 	story: 'After finishing 2nd in Rookie of the Year voting with the Marlins, Stark was traded to the Mariners before his sophomore season. Known for his speed, in 2020 he broke the single season record for stolen bases with 164, finishing the year with a .965 stolen base percentage. In 2022, Stark made his 4th straight All Star appearance as the starting 3rd Baseman.',
+				team: 'Seattle Mariners&trade;',
+				logo: 'mariners.png',
+				position: '3B',
+				recordsType: 'batting',
+				cardStyle: 'topps2019',
+				cardNo: 795,
+				info1: 'Ht: 5\'8\" Wt: 160 Bats: Right Throws: Right',
+				info2: 'Drafted: Marlins #26-2018. Acq. Trade w/ Marlins, 2019.',
+				sources: 'MLB The Show 18 statistics page',
+				// isHorizontal: true,
+				statsYear1: {
+						year: 2019, tm: 'mia', g: 100, ab: 318, r: 73, h: 132, '2b': 23, '3b': 8, hr: 25, rbi: 68, avg: '.415', obp: '.412', slg: '.777', war: '9.0', sb: '<i><b>125</b></i>', cs: 8, bb: '0', so: '6'
+				},
+				statsYear2: {
+						year: 2020, tm: 'sea', g: 155, ab: 618, r: 104, h: 191, '2b': 28, '3b': 9, hr: 27, rbi: 80, avg: '.309', obp: '.314', slg: '.519',  war: '7.7', sb: '<i><b>164</b></i>', cs: 6, bb: '3', so: '70'
+				},
+				statsYear3: {
+						year: 2021, tm: 'sea', g: 160, ab: '<i><b>682</b></i>', r: 93, h: 186, '2b': 24, '3b': 14, hr: 33, rbi: 84, avg: '.273', obp: '.287', slg: '.494', war: '6.0', sb: '<i><b>139</b></i>', cs: 7, bb: '14', so: '77'
+				},
+				// statsYear4: {
+				// 		year: 2022, club: 'sea', g: 100, ab: 100, r: 100, h: 100, '2b': 10, '3b': 10, hr: 10, rbi: 10, sb: 100, avg: '.000', slg: '.000'
+				// },
+				totals: { //through 2021
+						year: 'totals', tm: '', g: 415, ab: 1618, h: 509, r: 270, '2b': 75, '3b': 31, hr: 85, rbi: 232, avg: '.315', obp: '.316', slg: '.557', war: '22.7', sb: 428, cs: 17, bb: '17', so: '153'
+				}
 			}
 		]
 	},
@@ -108,7 +136,7 @@ new Vue({
 		}
 	},
 	methods: {
-		flipCard(player) {
+		flipCard(player, event) {
 			let playerName = player.name;
 			if (event.target.tagName.toLowerCase() != 'a') {
 				document.querySelector(`[data-player="${playerName}"]`).classList.toggle('back-shown');
