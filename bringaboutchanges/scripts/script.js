f85eea05f56c9542f8c23d1f4f21e7faed5c0a2c
Kyle Stark <kyleastark@gmail.com>
2020-11-21 19:26:47 -0700

    A few updates
    - updates to Bring About change site
    - adding red album covers to /h
    - adding http: -> https: redirect to htaccess

--- bringaboutchanges/scripts/script.js
+++ bringaboutchanges/scripts/script.js
@@ -1,14 +1,17 @@
 var menuButton = document.querySelector('.menu-button'),
-    menuIcon = document.querySelector('.menu'),    headerNav = document.querySelector('.header-nav');
+    menuIcon = document.querySelector('.menu'),    headerNav = document.querySelector('.header-nav'),
+    body = document.body;
 
 menuButton.addEventListener('click', function(){
     if (menuIcon.classList.contains('close-icon')) {
         headerNav.classList.add('mobile-closed');
         headerNav.classList.remove('mobile-open');
         menuIcon.classList.remove('close-icon');
+        body.classList.remove('menu-open');
     } else {
         headerNav.classList.remove('mobile-closed');
         headerNav.classList.add('mobile-open');
         menuIcon.classList.add('close-icon');
+        body.classList.add('menu-open');
     }
 });
