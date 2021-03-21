11171cf04f8fb4067ef8a220439215ea861593cb
Kyle Stark <kyleastark@gmail.com>
2021-03-20 23:38:33 -0700

    Changed filter in a few ways:
    - shows just first 151 pokemon by default so page loads quicker
    - new 'show all' button at top and bottom if you do want to see them all
    - clicking on a pokemon type will still filter by that type, but now clicking a second type will look for both (dual type)
    - clicking a 3rd type will start new filter with just that type, removing other 2 types

--- pkmnlist/scripts/pokemon.js
+++ pkmnlist/scripts/pokemon.js
@@ -2,6 +2,7 @@ new Vue({
 	el: '#app',
 	data: {
 		query: '',
+		pokemonTypes: ['bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water'],
 		pokemon: [
 			{ number: '1', name: 'Bulbasaur', stringname: '001', type1: 'grass', type2: 'poison' },
 			{ number: '2', name: 'Ivysaur', stringname: '002', type1: 'grass', type2: 'poison' },
@@ -1051,20 +1052,35 @@ new Vue({
 		]
 	},
 	computed: {
+		//filter list based on whether query appears in name, alt, type1, type2, or fits the format type1+type2 or type2+type1
 		filteredList () {
-			return this.pokemon.filter(pokemon => {
-				return pokemon.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
-					|| (pokemon.alt ? pokemon.alt.toLowerCase().indexOf(this.query.toLowerCase()) > -1 : '')
-					|| pokemon.type1.indexOf(this.query.toLowerCase()) > -1
-					|| pokemon.type2.indexOf(this.query.toLowerCase()) > -1
-					|| (pokemon.type1 + '+' + pokemon.type2).toString().indexOf(this.query.toLowerCase()) > -1
-					|| (pokemon.type2 + '+' + pokemon.type1).toString().indexOf(this.query.toLowerCase()) > -1
-			})
+			if (this.query == 'all') {
+				return this.pokemon.filter(pokemon => {
+					return pokemon.number > 0
+				})
+			} else if (this.query !== '') {
+				return this.pokemon.filter(pokemon => {
+					return pokemon.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
+						|| (pokemon.alt ? pokemon.alt.toLowerCase().indexOf(this.query.toLowerCase()) > -1 : '')
+						|| pokemon.type1.indexOf(this.query.toLowerCase()) > -1
+						|| pokemon.type2.indexOf(this.query.toLowerCase()) > -1
+						|| (pokemon.type1 + '+' + pokemon.type2).toString().indexOf(this.query.toLowerCase()) > -1
+						|| (pokemon.type2 + '+' + pokemon.type1).toString().indexOf(this.query.toLowerCase()) > -1
+				})
+			} else {
+				//if there's no query, only show first generation so page doesn't take forever to load all pokemon
+				return this.pokemon.filter(pokemon => {
+					return pokemon.number <= 151
+				})
+			}
 		}
 	},
 	methods: {
 		cancelSearch() {
 			this.query = '';
+		},
+		showAll() {
+			this.query = 'all';
 		},
 		createPkmnLink(number, name, stringname) {
 			var href = '';
@@ -1081,18 +1097,17 @@ new Vue({
 			}
 			return href;
 		},
-		createTypeLink(typeClicked) {
-			console.log('typeClicked :>> ', typeClicked);
-			//left off:
-			//define array of all types
-			//if query already contains one of those types, then add a + to the query, and the type that was clicked (which should show dual types)
-			//if not, then just change the query to the type that was clicked
-			//test so it doesn't do something like grass+grass
-
-			//style the button focus states
+		//if a pokemon type is clicked, filter by that type. if there's already a type in the filter, add it to the query (to search for dual types)
+		createTypeQuery(typeClicked) {
+			if (this.pokemonTypes.includes(this.query) && this.query !== typeClicked) {
+				this.query = this.query + '+' + typeClicked;
+			} else {
+				this.query = typeClicked;
+			}
 		}
 	},
 	beforeMount() {
+		//if there's a query string in the url, filter with that query
 		let url = window.location.href;
 		if (url.indexOf('?q=') > -1) {
 			this.query = url.substr((url.indexOf('?q='))+3, url.length);
