d7a89003ebea1bffb40a8e67ea59d5f75144e8a9
Kyle Stark <kyleastark@gmail.com>
2021-03-29 15:09:38 -0700

    Changing way of setting cookies

--- pkmnlist/scripts/pokemon.js
+++ pkmnlist/scripts/pokemon.js
@@ -1105,6 +1105,28 @@ new Vue({
 		showAll() {
 			this.query = 'all';
 		},
+		setCookie(cookieName, value, days) {
+			var expires = '';
+			if (days) {
+				var date = new Date();
+				date.setTime(date.getTime() + (days*24*60*60*1000));
+				expires = '; expires=' + date.toUTCString();
+			}
+			document.cookie = cookieName + '+' + (value || '')  + expires + '; path=/';
+		},
+		getCookie(cookieName) {
+			var nameEQ = cookieName + '+';
+			var ca = document.cookie.split(';');
+			for(var i=0;i < ca.length;i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1,c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			}
+			return null;
+		},
+		eraseCookie(cookieName) {   
+			document.cookie = cookieName +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+		},
 		changeLinkPreference() {
 			var linkPrefValue = document.querySelector('[name="link-preference"]:checked').value.toLowerCase();
 			this.linkPreference = linkPrefValue;
