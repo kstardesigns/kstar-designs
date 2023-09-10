13478a8c270c525990ddb3ffaaddea6d896c0ee0
Kyle <kyleastark@gmail.com>
2023-09-10 13:18:57 -0700

    Set up watcher for JS

--- yahtzee/gulpfile.js
+++ yahtzee/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const { watch, series } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
 
 async function minifycss() {
   return gulp.src('./*.scss')
@@ -10,7 +11,20 @@ async function minifycss() {
     .pipe(gulp.dest('dist'));
 }
 
+async function minifyjs() {
+  return gulp.src('./scripts.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+}
+
+gulp.task('minifyjs', function(){
+  return gulp.src('src/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('public/js'))
+});
+
+
 exports.default = function() {
   watch('*.scss', minifycss);
-  //watch('src/*.js', series(clean, javascript));
+  watch('*.js', minifyjs);
 };
