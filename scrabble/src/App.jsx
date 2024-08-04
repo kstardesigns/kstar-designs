80966fa6ff8ff06a75c34530cbadc30defc1ef64
Kyle <kyleastark@gmail.com>
2024-08-04 12:45:32 -0700

    Showing individual letter scores and adding up total

--- scrabble/src/App.jsx
+++ scrabble/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './styles.scss'; 
@@ -6,38 +6,60 @@ import './styles.scss';
 function App() {
   const [count, setCount] = useState(0);
   const [wordValue, setWordValue] = useState('');
-  const letters1 = 'eaionrtlsu';
-  const letters2 = 'dg';
-  const letters3 = 'bcmp';
-  const letters4 = 'fhvwy';
-  const letters5 = 'k';
-  const letters8 = 'jx';
-  const letters10 = 'qz';
-  const letters0 = ' ';
-
-
+  let [total, setTotal] = useState(0);
+  const letterScores = {
+    letters1: 'eaionrtlsu',
+    letters2: 'dg',
+    letters3: 'bcmp',
+    letters4: 'fhvwy',
+    letters5: 'k',
+    letters8: 'jx',
+    letters10: 'qz',
+    letters0: ' ',
+  };
 
   const handleWordChange = (event) => {
-    setWordValue(event.target.value);
+    setWordValue(event.target.value.toLowerCase());
   }
 
+  const getLetterScore = (letter) => {
+    for (const [key, value] of Object.entries(letterScores)) {
+      if (value.includes(letter)) {
+        return Number(key.replace('letters',''));
+      }
+    }
+    return 0;
+  }
 
+  useEffect(() => {
+    let newTotal = 0;
+    wordValue.split('').forEach((letter) => {
+      newTotal += getLetterScore(letter);
+    });
+    setTotal(newTotal);
+
+  },[wordValue]);
 
   return (
     <div>
 
       <div className="wrap">
-        <input type="text" className="word" autoFocus value={wordValue} onChange={handleWordChange} />
-        <div>
+        <div className="output">
           {
           wordValue.split("").map((letter, index) => (
-            <span key={index} className="letter">{letter}</span>
-          ))}
+            <div key={index} className="tile">
+              <span className="letter">{letter}</span>
+              <span className="value">{getLetterScore(letter)}</span>
+            </div>
+          ))
+          }
         </div>
+        <input type="text" className="word" autoFocus value={wordValue} onChange={handleWordChange} />
       </div>
+      <div className="total">total: {total}</div>
 
 
-      <div>
+      <div className="reactstuff">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
