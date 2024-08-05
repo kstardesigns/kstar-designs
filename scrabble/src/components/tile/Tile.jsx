765367fc2302ab26a9efe6fb1c63d7a47a7dca55
Kyle <kyleastark@gmail.com>
2024-08-05 12:11:52 -0700

    Added tile rotation, started gradient styling

--- scrabble/src/components/tile/Tile.jsx
+++ scrabble/src/components/tile/Tile.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react';
 import './_tile.scss'; 
 
 const Tile = ({letter, number}) => {
+    const tiltTile = () => {
+        const degrees = Math.random();
+        if (degrees < 0.66) {
+            return '0deg';
+        } else if (degrees >= 0.66 && degrees < 0.83) {
+            return '-2deg';
+        } else {
+            return '1deg';
+        }
+    }
 
     return (
-        <div className="tile">
+        <div className="tile" style={{ transform: `rotate(${tiltTile()})`}}>
             <span className="letter">{letter}</span>
             <span className="value">{number == 0 ? '' : number}</span>
         </div>
