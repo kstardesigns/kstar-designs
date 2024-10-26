58f7c6ffb89c4fdc0317a63a0542f2668c046c1b
Kyle <kyleastark@gmail.com>
2024-10-26 16:33:01 -0700

    Functioning site, needs more teams and styling

--- teamschemes/src/App.jsx
+++ teamschemes/src/App.jsx
@@ -1,28 +1,72 @@
 import { useState } from 'react';
+import colorData from './colors.json';
 import './_styles.scss'; 
 
 function App() {
 
-  const activeColors = [
-    { hex: '#0c233f', name: 'midnight navy blue' },
-    { hex: '#266092', name: 'lake blue' },
-    { hex: '#79bc43', name: 'aurora green' },
-    { hex: '#a0a2a3', name: 'moonlight grey' }
-  ];
+  const leagues = colorData.allColors;
+  const wnba = colorData.allColors.wnba;
+  let [currentTeam, setCurrentTeam] = useState(wnba.find(team => team.id === 'lynx')); 
+  //TODO: change this to random team on load
+
+  const changeColor = (league, teamId) => {
+    const newTeam = colorData.allColors[league].find(team => team.id === teamId);
+    setCurrentTeam(newTeam);
+  }
 
   return (
     <>
-      <div className={`grid grid--${activeColors.length}`}>
-        {
-          activeColors.map((color, index) => (
-            <div key={ index } style={{ backgroundColor: color.hex }}>
-              { color.hex } / { color.name }
+      <div className={ `grid grid--${currentTeam.colors.length}` }>
+      {
+        currentTeam.colors.map((color, colorIndex) => (
+          <div
+            key={ colorIndex }
+            style={{ backgroundColor: color.hex }}
+          >
+            { color.name }
+          </div> 
+        ))
+      }
+      </div>
+
+      <div className="sidebar">
+      {
+        /* sample full loop of all teams in a league */
+
+        Object.keys(leagues).map((leagueKey, leagueIndex) => (
+          <details key={leagueIndex} open>
+            {/* Display the name of the league */}
+            <summary>{ leagueKey.toUpperCase() }</summary>
+            <div>      
+              <ul>  
+                { leagues[leagueKey].map((team, teamIndex) => (
+
+                  <li key={ teamIndex }> 
+                    <button type="button" onClick={() => changeColor(leagueKey, team.id)} > { /* add click function and pass league and team id */ }
+                      <img src={ team.logo } alt={`${ team.name } logo`} style={{ width: '50px' }} />
+                      <span>{team.name}</span>
+                      { team.colors.map((color, colorIndex) => (
+                        <span
+                          key={ colorIndex }
+                          style={{ backgroundColor: color.hex, padding: '10px', margin: '5px' }}
+                        >
+                        
+                        </span>
+                      )) }
+                    </button>
+                  </li>
+                  
+                  )) 
+                }
+              </ul>
             </div>
-          ))
-        }
+
+          </details>
+        ))
+      }
       </div>
     </>
   )
 }
 
-export default App
+export default App;
